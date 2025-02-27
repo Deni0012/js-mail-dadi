9590f8eb91331e807a8a88115e2e43f0ab5535b7
Deni0012 <deni.dg00@icloud.com>
2025-02-27 15:06:00 +0100

    feat: creazione userDado

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -2,3 +2,8 @@
 
 // Generare numero random per il computer
 const computerDado = Math.floor(Math.random() * 6) + 1;
+
+// Chiedere all'utente di inserire un numero tra 1 e 6
+const userDado = parseInt(prompt("Inserisci un numero tra 1 e 6:"), 10);
+
+
