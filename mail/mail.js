9b2b89be4c098d3b2c3fbd12255eb0352aacea17
Deni0012 <deni.dg00@icloud.com>
2025-02-27 14:55:50 +0100

    feat: creazione email

--- mail/mail.js
+++ mail/mail.js
@@ -1,1 +1,12 @@
+// Crea una lista di email di invitati ad una festa.
+
+// Lista di email autorizzate
+const emailList = [
+    "mario@gmail.com",
+    "luigi@gmail.com",
+    "peach@gmail.com",
+    "toad@gmail.com",
+    "yoshi@gmail.com"
+];
+
 
