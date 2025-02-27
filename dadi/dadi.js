68472ba58a9450da9a60d85e27879fc0837564dd
Deni0012 <deni.dg00@icloud.com>
2025-02-27 15:06:47 +0100

    feat: verifica dell'input

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -6,4 +6,7 @@ const computerDado = Math.floor(Math.random() * 6) + 1;
 // Chiedere all'utente di inserire un numero tra 1 e 6
 const userDado = parseInt(prompt("Inserisci un numero tra 1 e 6:"), 10);
 
-
+// Verificare che l'input sia valido
+if (userDado < 1 || userDado > 6 || isNaN(userDado)) {
+    console.log("Numero non valido. Ricarica la pagina e inserisci un valore tra 1 e 6.");
+} 
